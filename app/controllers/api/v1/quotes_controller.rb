92558cc4544940ec0d91ffc6c830a6a455789c9f
ioilmio <illuminato.salvatore@gmail.com>
2020-09-18 00:14:40 +0200

    controller and view-working version

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,25 +1,45 @@
 class Api::V1::QuotesController < ApiController
-    before_action :set_quote
 
     def index
-        # @quotes = Quote.all
-        render json: @quotes
+        @quotes = Quote.all
+        render json: @quotes, status: :ok
     end
 
     def show
-        # @quote = Quote.find(params[:id])
-        render json: @quote
+        set_quote
+        render json: @quote, status: :ok
     end
 
-    # def destroy
-    #     @quote = Quote.find(params[:id])
-    #     @quote.destroy
-    # end
+    def create 
+        @quote = Quote.new(quote_params)
+        @quote.save
+        render json: @quote, status: :created
+    end
+
+    def update
+        set_quote
+        @quote.update_attributes(quote_params)
+        @quote.save
+        render json: @quote, status: :ok        
+    end
+
+    def destroy
+        set_quote
+        if @quote.destroy
+            render json: @quote, status: :ok
+            head :ok
+        else
+            head :unprocessable_entity
+        end
+    end
 
     private 
-    
+    def quote_params
+        params.require(:quote).permit(:author, :sentence)
+    end
+
+
     def set_quote
         @quote = Quote.find(params[:id])
-        @quotes = Quote.all
     end
 end
