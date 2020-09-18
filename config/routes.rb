ada9355dcc16590c42e485bdb2da4859d14ce5bd
ioilmio <illuminato.salvatore@gmail.com>
2020-09-18 19:40:23 +0200

    controller and routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :quotes
+      resource :quotes do
+      get 'index'
+      get 'random'
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
